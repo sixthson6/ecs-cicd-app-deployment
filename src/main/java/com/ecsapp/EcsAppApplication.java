42ad5c59e465c072f2539c96920b05fdbd06485f
sixthson6 <sudais2408@gmail.com>
2025-09-10 10:53:14 +0000

    first commit

---
+++ src/main/java/com/ecsapp/EcsAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcsAppApplication.class, args);
+    }
+
+}
